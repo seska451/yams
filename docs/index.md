2c898e97636892c6a5c971d72deea2530f9b7eac
seska451 <56018242+seska451@users.noreply.github.com>
2023-07-18 17:05:42 +0930

    Added documentation to the code

--- docs/index.md
+++ docs/index.md
@@ -44,7 +44,22 @@ If you want to include yams in your next mission, its as simple as:
 3. Open your mission editor and either create a new mission or create a new one
 4. In the mission editor, click the triggers manager
 5. Create a new `trigger`, giving it a `condition` to wait for 1 second. Set the `action` to `DO SCRIPT FILE` and select the `yams-dcs.lua` file from your filesystem
-6. Start your mission. You'll see that YAMS outputs a message ot the screen for you to indicate that it is working.
+6. Start your mission. Yams is now loaded but how can you tell? We will need to write our first in game script for that.
+
+### Adding your first script
+
+Now that yams is available for your use, your code can use the library in subsequent scripts.
+
+Create a new trigger and use a `FLAG ON condition` checking for flag 31337, and use the `DO SCRIPT` action this time.
+
+In that `DO SCRIPT` block, paste in the following code:
+
+```lua
+yams.message
+   :with_text("YAMS v0.1 loaded.")
+   :for_seconds(15)
+   :clear_previous_messages()
+```
 
 ###### _The DCS Mission editor, highlighting the trigger button_
 ![The DCS Mission editor, highlighting the trigger button](/assets/open-triggers.png)
--- src/yams.lua
+++ src/yams.lua
@@ -1,41 +1,60 @@
--- Messages
+--- @script yams-dcs.lua
+--- Description; The YAMS API script which you can use for mission scripting in DCS World.
+--- It has no other dependencies other than the Server Side Scripting Engine (SSSE) used by DCS itself, so you dont need
+--- To install other scripts to make use of it.
+
 yams = {}
 
+--- The messaging module. This module contains a fluent interface for constructing and displaying message content on the RHS of screen, and in the chat.
+--- @module message
 local message = {
     text = nil,
     time = 10,
     should_clear = false
 }
 
+--- Adds text to an ephemeral message shown to the user on the upper RHS of the screen.
+--- @param txt the text to be sent.
+--- @returns a message object.
 function message:with_text(txt)
     self.text = txt
-    return message
+    return self
 end
 
+--- Sets the message duration
+--- @param for_seconds the number of seconds to apply the message to the screen before it is erased.
+--- @returns a message object.
 function message:for_seconds(seconds)
     self.time = seconds
     return self
 end
 
+--- Upon displaying this message, remove all other messages
+--- @returns a message object.
 function message:clear_previous_messages()
     self.should_clear = true
     return self
 end
 
+--- Send the ephemeral message to all users on the Upper RHS Screen area.
 function message:send()
     trigger.action.outText(self.text, self.time, self.should_clear)
     return self
 end
 
-
 -- End Messages
+
 -- Flags
+--- The flag module exposes a fluent interface for dealing with flags
+--- @module flag
 local flag = {
     flag_index = 0,
     value = 0
 }
 
-
+--- Sets the value of a flag for a given index.
+--- @param ndx The flags index number.
+--- @param value The value to set the flag. 0 means off, or false. 1 or higher means on, or true.
 function flag:set_value(ndx, value)
     self.flag_index = ndx
     self.value = value
@@ -43,6 +62,8 @@ function flag:set_value(ndx, value)
     return self
 end
 
+--- Sets a flag for a given index, to ON.
+--- @param ndx The flags index number.
 function flag:set(ndx)
     self.flag_index = ndx
     self.value = true
@@ -50,6 +71,8 @@ function flag:set(ndx)
     return self
 end
 
+--- Sets a flag for a given index, to OFF.
+--- @param ndx The flags index number.
 function flag:unset(ndx)
     self.flag_index = ndx
     self.value = false
@@ -58,18 +81,14 @@ function flag:unset(ndx)
 end
 -- end Flags
 
+--- This is the root module that acts as the entry point to all yams functionality.
+--- @module yams
 yams = {
     message = message,
     flag = flag
 }
 
-message
-    :with_text("YAMS v0.1 loaded.")
-    :for_seconds(20)
-    :clear_previous_messages()
-    :send()
-
-flag
-    :set(31337, true)
+-- let the server know that yams has been loaded via this flag
+yams.flag:set(31337)
 
 return yams
